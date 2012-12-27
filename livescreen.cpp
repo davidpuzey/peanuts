aae506f7401cfa77d6f23f24bfd6d52ac0d6dc76
David Puzey <david.puzey@gmail.com>
2012-12-27 23:36:00 +0000

    Moved some code and the cursor is now invisible over livescreen.

--- livescreen.cpp
+++ livescreen.cpp
@@ -2,10 +2,8 @@
 #include "livescreen.h"
 
 LiveScreen::LiveScreen() {
-	QPalette palette;
-	palette.setBrush(QPalette::Background,QPixmap("background.jpg").scaled(QApplication::desktop()->screenGeometry().size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation));
-	setPalette(palette);
 	setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint); // Setting up for full screen (on show we set the position of the window)
+	setCursor(QCursor(Qt::BlankCursor));
 	setWindowTitle(tr("Live Screen"));
 }
 
@@ -24,6 +22,13 @@ void LiveScreen::closeEvent(QCloseEvent *event) {
  * Ensure that the window is full screen
  */
 void LiveScreen::showEvent(QShowEvent *event) {
-	setGeometry(QApplication::desktop()->screenGeometry());
+	QPalette palette;
+	QRect sGeometry = QApplication::desktop()->screenGeometry(1);
+	QPixmap bgimage = QPixmap("background.jpg").scaled(sGeometry.size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+	bgimage.scroll((sGeometry.width()-bgimage.width())/2, (sGeometry.height()-bgimage.height())/2, bgimage.rect());
+	palette.setBrush(QPalette::Background,bgimage);
+	setPalette(palette);
+	
+	setGeometry(sGeometry);
 	event->accept();
 }
