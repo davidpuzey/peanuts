9523adf0dcd034637b1f71bcb8d0c4d683b1002e
David Puzey <david.puzey@gmail.com>
2013-02-01 17:09:30 +0000

    Improved the bones .. still bare bones but at least they mean something. Using labels to place numbers.

--- modules/PrizeBoardModule.cpp
+++ modules/PrizeBoardModule.cpp
@@ -16,10 +16,11 @@ PrizeBoardControl::PrizeBoardControl() {
 	int cellSwitch = noButtons - (noButtons % cols); // The cell at which the last row starts
 	
 	for (int i = 0; i < noButtons; i++) {
+		QPushButton *button = new QPushButton(QString::number(i+1), this);
 		if (i < cellSwitch)
-			layout->addWidget(new QPushButton(QString::number(i+1), this), i / cols, i % cols); // To work out rows we divide the current item number by the number of items. To work out columns we take the remainder from the devision (ie use modulus).
+			layout->addWidget(button, i / cols, i % cols); // To work out rows we divide the current item number by the number of items. To work out columns we take the remainder from the devision (ie use modulus).
 		else
-			lastRow->addWidget(new QPushButton(QString::number(i+1), this));
+			lastRow->addWidget(button);
 	}
 	
 	if (cellSwitch != 0)
@@ -29,5 +30,23 @@ PrizeBoardControl::PrizeBoardControl() {
 }
 
 PrizeBoardLive::PrizeBoardLive() {
-	QPushButton("Stuff", this);
+	QGridLayout *layout = new QGridLayout(this);
+	QHBoxLayout *lastRow = new QHBoxLayout();
+	
+	int noButtons = 24;
+	int cols = qCeil(qSqrt(noButtons)); // The closest square root value (works out best fit for the items)
+	int cellSwitch = noButtons - (noButtons % cols); // The cell at which the last row starts
+	
+	for (int i = 0; i < noButtons; i++) {
+		QLabel *label = new QLabel(QString::number(i+1), this);
+		if (i < cellSwitch)
+			layout->addWidget(label, i / cols, i % cols); // To work out rows we divide the current item number by the number of items. To work out columns we take the remainder from the devision (ie use modulus).
+		else
+			lastRow->addWidget(label);
+	}
+	
+	if (cellSwitch != 0)
+		layout->addLayout(lastRow, layout->rowCount(), 0, 1, cols); // Add the last row in seperately, this allows us to centre the buttons is there are less than the number of columns
+	
+	setLayout(layout);
 }
