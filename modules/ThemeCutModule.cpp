ce1d271fca8d475496a4fe3d9e67680e1d0e8e31
David Puzey <david.puzey@gmail.com>
2013-02-26 15:13:44 +0000

    Added a music cut thingy. Play theme plays a theme, in this case James Bond, and the Cut button stops it and plays the cut sound, in this case a record scratch.

---
+++ modules/ThemeCutModule.cpp
@@ -0,0 +1,37 @@
+#include <ctime>
+#include <QtGui>
+#include "ThemeCutModule.h"
+
+ThemeCutModule::ThemeCutModule() {
+	setTitle("Theme Cut");
+	setControlWidget(new ThemeCutControl);
+	setLiveWidget(new ThemeCutLive);
+	BaseControl *control = getControlWidget();
+	BaseLive *live = getLiveWidget();
+}
+
+ThemeCutControl::ThemeCutControl() {
+	QHBoxLayout *layout = new QHBoxLayout();
+	
+	QPushButton *theme = new QPushButton("Play Theme", this);
+	QPushButton *cut = new QPushButton("Cut", this);
+	
+	layout->addWidget(theme);
+	layout->addWidget(cut);
+	
+	connect(theme, SIGNAL(clicked()), this, SLOT(playTheme()));
+	connect(cut, SIGNAL(clicked()), this, SLOT(playCut()));
+	
+	setLayout(layout);
+}
+
+void ThemeCutControl::playTheme() {
+	QSound::play("theme.wav");
+}
+
+void ThemeCutControl::playCut() {
+	QSound::play("cut.wav");
+}
+
+ThemeCutLive::ThemeCutLive() {
+}
---
+++ modules/ThemeCutModule.h
@@ -0,0 +1,30 @@
+#include <QtGui>
+#include "BaseModule.h"
+
+typedef QLabel* QLabelArray;
+
+class ThemeCutModule : public BaseModule {
+	Q_OBJECT
+	
+	public:
+		ThemeCutModule();
+};
+
+class ThemeCutControl : public BaseControl {
+	Q_OBJECT
+	
+	public:
+		ThemeCutControl();
+	
+	public slots:
+		void playTheme();
+		void playCut();
+};
+
+class ThemeCutLive : public BaseLive {
+	Q_OBJECT
+	
+	
+	public:
+		ThemeCutLive();
+};
