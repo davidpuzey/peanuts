834596f299c7563d7d9638e32795c96bc95abc9b
David Puzey <david.puzey@gmail.com>
2014-10-14 21:49:25 +0100

    Questions module

--- modules/QuestionsModule.cpp
+++ modules/QuestionsModule.cpp
@@ -11,7 +11,48 @@ QuestionsModule::QuestionsModule() {
 }
 
 QuestionsControl::QuestionsControl() {
+	QHBoxLayout *layout = new QHBoxLayout();
+    QListWidget *questionList = new QListWidget(this);
+    QFile file("files/questions");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    while (!file.atEnd()) {
+        QByteArray background = file.readLine();
+        background.chop(1);
+        QByteArray line = file.readLine();
+        line.chop(1);
+        if (line != "")
+            new QListWidgetItem(line, questionList);
+        else
+            new QListWidgetItem(background, questionList);
+    }
+
+
+    layout->addWidget(questionList);
+    setLayout(layout);
 }
 
 QuestionsLive::QuestionsLive() {
+    QHBoxLayout *layout = new QHBoxLayout();
+    QLabel *text = new QLabel();
+    text->setAlignment(Qt::AlignCenter);
+    QFont font = text->font();
+    font.setPointSize(72);
+    text->setFont(font);
+    QPalette palette = text->palette();
+    palette.setColor(text->foregroundRole(), Qt::black);
+    text->setPalette(palette);
+    text->setStyleSheet("background-image: url(./media/images/PrizeBronze.png); background-repeat: no-repeat;");
+    text->setText("Testing Stuff");
+    layout->addWidget(text);
+/*	QPalette palette;
+	QRect sGeometry = QApplication::desktop()->screenGeometry(1);
+	QPixmap bgimage = QPixmap("media/images/PrizeBronze.png").scaled(sGeometry.size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+	bgimage.scroll((sGeometry.width()-bgimage.width())/2, (sGeometry.height()-bgimage.height())/2, bgimage.rect());
+	palette.setBrush(QPalette::Background,bgimage);
+	setPalette(palette);
+
+	setGeometry(sGeometry);*/
+    setLayout(layout);
 }
